7a3ff2a0c588abf24e9118bac79549eaa5409f50
Borja Arrieta <barrieta@gestion400.com>
2020-08-26 03:01:47 +0200

    Primer Comit

---
+++ src/com/gswrapper/control/AccionManager.java
@@ -0,0 +1,124 @@
+package com.gswrapper.control;
+
+import java.lang.reflect.*;
+
+import org.apache.log4j.*;
+
+import com.gswrapper.modelo.acciones.*;
+import com.gswrapper.modelo.dao.*;
+import com.gswrapper.modelo.vista.*;
+import com.gswrapper.modelo.wrapper.*;
+import com.gswrapper.util.*;
+
+/**
+ * 
+ * AccionManager.java
+ *
+ * @author Borja Arrieta Zueco
+ * @version 1.3v
+ * @date 2019-07-19
+ *
+ */
+
+public class AccionManager {
+	
+	private static final Logger LOGGER = Logger.getLogger(AccionManager.class);
+	
+	private static final String METODO_ACCION = "ejecutar";
+	private static final String MENSAJE_ACCION = "Ejecutando la accion %s";
+	
+	private static AccionManager instance;
+	
+	private Dao<MetaAccion> accionDao = AccionDao.getInstance();
+	
+	public static synchronized AccionManager getInstance() {
+		
+		if(instance == null) instance = new AccionManager();
+		
+		return instance;
+	}
+	
+	
+	public boolean isAccionValida(String nombreAccion, Object parametros) {
+		
+		return existeAccion(nombreAccion) && parametrosValidos(accionDao.get(nombreAccion), parametros);
+	}
+	
+	public Pantalla getPantallaAccion(String nombreAccion) {
+		
+		Pantalla pantalla = null;
+		
+		if(existeAccion(nombreAccion)) {
+			
+			Dao<Pantalla> pantallaDao = PantallaDao.getInstance();
+			
+			pantalla = pantallaDao.get(accionDao.get(nombreAccion).getPantalla());
+		}
+		
+		return pantalla;
+	}
+	
+	public ResultadoAccion ejecutarAccion(String nombreAccion, Object parametros, Object ... parametrosConstructor) {		
+		
+		LogUtil.info(LOGGER, String.format(MENSAJE_ACCION, nombreAccion));
+			
+		MetaAccion metaAccion = accionDao.get(nombreAccion);
+				
+		Class<? extends AccionBase<?, ?>> claseAccion = metaAccion.getClase();
+
+		Constructor<? extends AccionBase<?, ?>> constructor = getConstructor(claseAccion);
+		
+		AccionBase<?, ?> accion = newInstance(constructor, parametrosConstructor);
+		
+		Method metodoEjecutar = ClassUtil.getMethod(claseAccion, METODO_ACCION, metaAccion.getClaseEntrada());
+		
+		ResultadoAccion resultadoAccion = new ResultadoAccion();
+		
+		resultadoAccion.setResultado(ClassUtil.invoke(accion, metodoEjecutar, parametros));
+		
+		resultadoAccion.setPantallasAbiertas(accion.getPantallasAbiertas());
+			
+		return resultadoAccion;
+	}
+	
+	private Constructor<? extends AccionBase<?, ?>> getConstructor(Class<? extends AccionBase<?, ?>> claseAccion) {
+		
+		return ClassUtil.getConstructor(claseAccion, GSAWrapper3270.class, Pantalla.class);
+	}
+	
+	private AccionBase<?, ?> newInstance(Constructor<? extends AccionBase<?, ?>> constructor, Object ... parametros) {
+		
+		AccionBase<?, ?> instancia = null;
+		
+		try {
+			
+			instancia = constructor.newInstance(parametros);
+			
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+		
+			LOGGER.error(String.format(ClassUtil.ERROR_INSTANCIA, constructor.getName()), e);
+		}
+		
+		return instancia;
+	}
+	
+	private boolean existeAccion(String nombreAccion) {
+		
+		return accionDao.exists(nombreAccion);
+	}
+	
+	
+	private boolean parametrosValidos(MetaAccion accion, Object parametros) {
+		
+		boolean parametrosValidos = true;
+		
+		if(!Condition.empty(parametros)) {
+			
+			parametrosValidos = parametros.getClass().equals(accion.getClaseEntrada());
+		}
+		
+		return parametrosValidos;		
+	}
+	
+}
---
+++ src/com/gswrapper/modelo/wrapper/GSAWrapper3270.java
@@ -0,0 +1,25 @@
+package com.gswrapper.modelo.wrapper;
+
+import com.gswrapper.excepciones.*;
+import com.gswrapper.modelo.vista.*;
+
+/**
+ * 
+ * GSAWrapper3270.java
+ *
+ * @author Borja Arrieta Zueco
+ * @version 1.0v
+ * @date 2019-07-19
+ *
+ */
+public interface GSAWrapper3270 extends Wrapper3270 {
+
+	public void cargarPantallasXML(String pathXML) throws ClassNotFoundException;
+	public void abrirPantalla(String nombrePantalla) throws IllegalScreenException;
+	public void abrirPantalla(String nombrePantalla, boolean abrirPantallasAnteriores) throws IllegalScreenException;
+	public void abrirPantalla(Pantalla pantalla);
+	public void cerrarPantalla(String nombrePantalla) throws IllegalScreenException;
+	public void cerrarPantalla(Pantalla pantalla);
+	public Object ejecutarAccion(String nombreAccion, Object parametros) throws IllegalScreenException, IllegalActionExcepcion;
+	public Object ejecutarAccion(String nombreAccion, Object parametros, String pantallaEsperada) throws IllegalScreenException, IllegalActionExcepcion;
+}
